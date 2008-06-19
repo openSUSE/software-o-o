3ffd0826d98eeecbe96ffd9e1ea5a87948adf832
Andreas Bauer <abauer@suse.com>
2008-06-19 13:45:00 +0000

    - replaced dummy text - added mlasars to recipients again

--- app/models/promomailer.rb
+++ app/models/promomailer.rb
@@ -1,6 +1,6 @@
 class Promomailer < ActionMailer::Base
   def promo_order(order)
-    recipients  ["abauer@suse.de"]
+    recipients  ["abauer@suse.de", "mlasars@suse.de"]
     from        "admin@opensuse.org"
     subject     "openSUSE 11.0 PromoDVD order"
     body        :order => order
