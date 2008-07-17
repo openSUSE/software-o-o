bb996c03385ee720d934571fe645610bccea2058
Andreas Bauer <abauer@suse.com>
2008-07-17 16:01:10 +0000

    - changed subject of promodvd order mail

--- app/models/promomailer.rb
+++ app/models/promomailer.rb
@@ -2,7 +2,7 @@ class Promomailer < ActionMailer::Base
   def promo_order(order)
     recipients  ["abauer@suse.de", "mlasars@suse.de"]
     from        "admin@opensuse.org"
-    subject     "openSUSE 11.0 PromoDVD order"
+    subject     "openSUSE 11.0 PromoDVD ordertool"
     body        :order => order
   end
 end
