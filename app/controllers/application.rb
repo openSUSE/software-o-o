b6aabfcc3da2bd97f02ac53dbdeb0366f3eb6ffb
Andreas Bauer <abauer@suse.com>
2008-12-08 19:48:19 +0000

    - gzip-encode output

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,11 +1,35 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 
+require 'stringio'
+require 'zlib'
+
 class ApplicationController < ActionController::Base
   # Pick a unique cookie name to distinguish our session data from others'
   # session :session_key => '_software_session_id'
   session :disabled => true
   layout "application_no_js"
+
+  after_filter :compress
+
+  def compress
+    if (md = self.request.env['HTTP_ACCEPT_ENCODING'].match(/gzip;?(q=([^,]*))?/))
+      if md[1].nil? or md[2].to_f > 0.0
+        if self.response.headers["Content-Transfer-Encoding"] != 'binary'
+          logger.debug "compressing output"
+          begin 
+            ostream = StringIO.new
+            gz = Zlib::GzipWriter.new(ostream)
+            gz.write(self.response.body)
+            self.response.body = ostream.string
+            self.response.headers['Content-Encoding'] = 'gzip'
+          ensure
+            gz.close
+          end
+        end
+      end
+    end
+  end
 
   def rescue_action_in_public(exception)
     @message = exception.message
