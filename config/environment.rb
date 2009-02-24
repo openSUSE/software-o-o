76075fff3a60e147f9f45d783f1e5469698439b5
Adrian Schröter <adrian@suse.de>
2009-02-24 16:31:44 +0000

    set requirement to RoR 2.1.2

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,7 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 #RAILS_GEM_VERSION = '1.2.3' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.1.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
