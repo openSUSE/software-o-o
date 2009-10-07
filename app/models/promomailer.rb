8a813aa45166e88fa92445738ef243b37e874d25
Andreas Bauer <abauer@suse.com>
2009-10-07 14:30:40 +0000

    - fix recipient list

--- app/models/promomailer.rb
+++ app/models/promomailer.rb
@@ -1,6 +1,7 @@
 class Promomailer < ActionMailer::Base
   def promo_order(order)
-    recipients  ["abauer@suse.de", "mlasars@suse.de"]
+    # TODO: move to config/environment.rb
+    recipients  ["jbrockmeier@novell.com", "michl@suse.de"]
     from        "admin@opensuse.org"
     subject     "openSUSE 11.0 PromoDVD ordertool"
     body        :order => order
