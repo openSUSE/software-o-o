a95f7a271cc8b77e5e4523bf40f917fce4ab4d36
Stephan Kulow <coolo@suse.de>
2010-02-19 13:19:42 +0100

    add some memory logging support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 
+require 'memory_profiler'
+
 class ApplicationController < ActionController::Base
 
+  @memory = 0
+  before_filter :memory
+  after_filter :logit
+
   init_gettext('software')
+
+  def initialize
+    MemoryProfiler.start :string_debug => false
+  end
 
   def rescue_action_in_public(exception)
     @message = exception.message
@@ -12,6 +22,15 @@ class ApplicationController < ActionController::Base
     else
       render :template => 'error', :layout => "application", :status => 404
     end
+  end
+
+  def memory
+    @memory = `ps -o rss= -p #{$$}`.to_i
+  end
+
+  def logit
+    mu = `ps -o rss= -p #{$$}`.to_i
+    logger.debug "Request took #{mu-@memory} - now #{mu}"
   end
   
 end
