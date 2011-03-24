668ebdea4ab291c336a7ce354d32ea426f941d17
Thomas Schmidt <tschmidt@suse.de>
2011-03-24 18:26:40 +0100

    set reply-to

--- app/models/promomailer.rb
+++ app/models/promomailer.rb
@@ -2,7 +2,7 @@ class Promomailer < ActionMailer::Base
   def promo_order(order)
     # TODO: move to config/environment.rb
     recipients  ["promodvds@opensuse.org", "tschmidt@suse.de"]
-    from        "admin@opensuse.org"
+    from        order[:email]
     subject     "openSUSE PromoDVD order: #{order[:amount]} to #{order[:name]}"
     body        :order => order
   end
