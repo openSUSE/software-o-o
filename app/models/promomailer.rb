059d119709a40ab944cd0309074ff131fbb08dd0
Thomas Schmidt <tschmidt@suse.de>
2011-03-31 11:49:17 +0200

    remove myself from mails

--- app/models/promomailer.rb
+++ app/models/promomailer.rb
@@ -1,7 +1,7 @@
 class Promomailer < ActionMailer::Base
   def promo_order(order)
     # TODO: move to config/environment.rb
-    recipients  ["promodvds@opensuse.org", "tschmidt@suse.de"]
+    recipients  ["promodvds@opensuse.org"]
     from        order[:email]
     subject     "openSUSE PromoDVD order: #{order[:amount]} to #{order[:name]}"
     body        :order => order
