9c3ce699484884f5d9903976bb12021f94b99901
Pavol Rusnak <stick@gk2.sk>
2011-07-19 16:07:56 +0200

    use nice version sorting on download page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -65,3 +65,11 @@ module ApplicationHelper
 
 
 end
+
+module Enumerable
+  def version_sort
+    sort_by { |key,val|
+       key.gsub(/_SP/,'.').gsub(/_Factory/,'_100').split(/_/).map { |v| v =~ /\A\d+(\.\d+)?\z/ ? -(v.to_f) : v.downcase }
+    }
+  end
+end
