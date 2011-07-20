9d875245bf55073e4ede9d64fc250d07c2537e45
Pavol Rusnak <stick@gk2.sk>
2011-07-20 15:59:16 +0200

    fix version_sort for tumbleweed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -69,7 +69,11 @@ end
 module Enumerable
   def version_sort
     sort_by { |key,val|
-       key.gsub(/_SP/,'.').gsub(/_Factory/,'_100').split(/_/).map { |v| v =~ /\A\d+(\.\d+)?\z/ ? -(v.to_f) : v.downcase }
+       key.gsub(/_SP/, '.') \
+          .gsub(/_Factory/, '_100') \
+          .gsub(/_Tumbleweed/, '_99') \
+          .split(/_/) \
+          .map { |v| v =~ /\A\d+(\.\d+)?\z/ ? -(v.to_f) : v.downcase }
     }
   end
 end
