45b7bdfb2b1bb199974a89997b646dae61d74bd9
Thomas Schmidt <tschmidt@suse.de>
2012-03-09 12:45:19 +0100

    use image helper, add standard icon

--- app/helpers/package_helper.rb
+++ app/helpers/package_helper.rb
@@ -19,19 +19,39 @@ module PackageHelper
     end
   end
 
-   def shorten(text, chars)
-     text.length > chars ? text[0,chars] + "..." : text
+  def shorten(text, chars)
+    text.length > chars ? text[0,chars] + "..." : text
   end
 
-   def prepare_desc txt
-     txt = txt.gsub(/[\n][\n]+/, "\n\n")
-     txt = create_links txt
-     txt
-   end
+  def prepare_desc txt
+    txt = txt.gsub(/[\n][\n]+/, "\n\n")
+    txt = create_links txt
+    txt
+  end
 
   def create_links (txt)
     txt = txt.gsub(/(https?:\/\/[-_A-Za-z0-9\/\(\)\[\]:,.;?&+@#%=~|]+[^),. <"'\n\r])/m, '<a href="\1">\1</a> ')
     txt
+  end
+
+  def screenshot_thumb_url pkgname
+    case pkgname
+    when /-devel$/
+      screenshot_thumb =  image_path "file_settings.png"
+    when /-debug$/
+      screenshot_thumb = image_path "file_settings.png"
+    when /-doc$/
+      screenshot_thumb = image_path "file_settings.png"
+    when /-javadoc$/
+      screenshot_thumb = image_path "file_settings.png"
+    when /-debuginfo$/
+      screenshot_thumb = image_path "file_settings.png"
+    when /-debugsource$/
+      screenshot_thumb = image_path "file_settings.png"
+    else
+      screenshot_thumb = "http://screenshots.debian.net/thumbnail/" + pkgname
+    end
+    screenshot_thumb
   end
 
 
