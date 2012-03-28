ccefaaa3dafde7961ff011593ae28038f9ccee28
Thomas Schmidt <tschmidt@suse.de>
2012-03-28 15:34:23 +0200

    fix statistics

--- app/controllers/statistic_controller.rb
+++ app/controllers/statistic_controller.rb
@@ -14,7 +14,11 @@ class StatisticController < ApplicationController
     Rails.cache.fetch('top_searches', :expires_in => 2.hours) do
       result = ActiveRecord::Base.connection.execute("select query, count(*) as c from search_histories where query is NOT NULL " +
           "AND count > 0 AND created_at > '#{TIME_LIMIT}' group by query order by c desc limit 15;")
-      result.map{|entry| { :query => entry[0].strip.downcase, :count => entry[1].to_i} }
+      top = Array.new
+      result.each do |entry|
+        top << { :query => entry[0].strip.downcase, :count => entry[1].to_i}
+      end
+      top
     end
   end
 
@@ -23,8 +27,13 @@ class StatisticController < ApplicationController
     Rails.cache.fetch('failed_searches', :expires_in => 2.hours) do
       result = ActiveRecord::Base.connection.execute("select query, count(*) as c from search_histories where query is NOT NULL " +
           "AND count = 0 AND created_at > '#{TIME_LIMIT}' group by query order by c desc limit 15; ")
-      result.map{|entry| { :query => entry[0].strip.downcase, :count => entry[1].to_i} }
+      failed = Array.new
+      result.each do |entry|
+        failed << { :query => entry[0].strip.downcase, :count => entry[1].to_i}
+      end
+      failed
     end
   end
+
 
 end
