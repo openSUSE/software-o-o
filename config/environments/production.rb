4cfbf7ae4dc6f2b08b016a18864f45067b3c0744
Thomas Schmidt <tschmidt@suse.de>
2012-11-13 19:39:47 +0100

    set memcache store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,2 +1,6 @@
 CONFIG['use_static'] = "software.o.o"
 
+SoftwareOO::Application.configure do
+  config.cache_store = :mem_cache_store, 'localhost:11211', {namespace: 'software', compress: true}
+  config.log_level = :debug
+end
