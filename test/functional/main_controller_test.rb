caf0c8b35ebc77a7510b69a8208eca3b75444a19
Ancor Gonzalez Sosa <ancor@suse.de>
2013-02-28 19:29:13 +0100

    Small fix in main_controller_test

--- test/functional/main_controller_test.rb
+++ test/functional/main_controller_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class MainControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
-    assert_response :success
+    assert_response :redirect
   end
 end
