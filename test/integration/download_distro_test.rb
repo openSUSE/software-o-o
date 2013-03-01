a939e017f2c3af5a64e6ea12585a47dd40faf5e3
Ancor Gonzalez Sosa <ancor@suse.de>
2013-03-01 10:05:26 +0100

    Test if BitTorrent is disabled for network version

--- test/integration/download_distro_test.rb
+++ test/integration/download_distro_test.rb
@@ -22,5 +22,24 @@ class DownloadDistroTest < ActionDispatch::IntegrationTest
     click_button "download_button"
     assert_match /^http:\/\/download.*GNOME-LiveCD-i686.iso.meta4$/, evaluate_script("mylink")
   end  
+
+  def test_network_bittorrent
+    # Choose kde BitTorrent download
+    find('#ci_kde').click
+    choose 'BitTorrent'
+    assert has_checked_field?("p_torrent")
+    assert has_no_checked_field?("p_http")
+    assert_no_selector :xpath, "//input[@id='p_torrent'][@disabled='disabled']"
+    # Change to network install, so BitTorrent should disable
+    find('#ci_net').click
+    assert has_no_checked_field?("p_torrent")
+    assert has_checked_field?("p_http")
+    assert_selector :xpath, "//input[@id='p_torrent'][@disabled='disabled']"
+    # Choosing another option enables BitTorrent again
+    find('#ci_dvd').click
+    assert has_checked_field?("p_http")
+    assert_no_selector :xpath, "//input[@id='p_torrent'][@disabled='disabled']"
+  end
+
 end
 
