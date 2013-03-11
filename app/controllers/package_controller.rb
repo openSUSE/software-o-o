b57dbe282d9f6647db567e032e02608395ad5f0d
Ancor Gonzalez Sosa <ancor@suse.de>
2013-03-11 15:18:52 -0400

    beta warning removed from the packages/appstore section

--- app/controllers/package_controller.rb
+++ app/controllers/package_controller.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 
 class PackageController < ApplicationController
 
-  before_filter :set_beta_warning, :only => [:category, :categories]
+  #before_filter :set_beta_warning, :only => [:category, :categories]
   before_filter :set_search_options, :only => [:show, :categories]
   before_filter :prepare_appdata, :set_categories, :only => [:show, :categories, :category]
 
