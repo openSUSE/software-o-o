05b7291e705144d8b9020e501aac4ce01bff9ad1
Henne Vogelsang <hvogel@opensuse.org>
2017-04-25 19:30:17 +0200

    Remove the promo dvd order functionality
    We no longer make use of this

--- app/models/promomailer.rb
+++
@@ -1,6 +0,0 @@
-class Promomailer < ActionMailer::Base
-  def promo_order(order)
-    @order = order
-    mail(to: CONFIG['order_mails_to'], from: order[:email], subject: "openSUSE PromoDVD order: #{order[:amount]} to #{order[:name]}")
-  end
-end
