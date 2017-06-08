b3b04059d5458216c33a42cfb9c3d53378c2a059
Henne Vogelsang <hvogel@opensuse.org>
2017-06-08 17:00:11 +0200

    Switch to new site by default

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 SoftwareOO::Application.routes.draw do
 
+  root to: 'distributions#index'
+  get '/:locale', to: 'distributions#index'
+
   resources :distributions, only: [:index] do
     collection do
       get 'tumbleweed'
@@ -9,36 +12,6 @@ SoftwareOO::Application.routes.draw do
       get 'leap/ports', to: 'distributions#leap_ports'
       get 'testing'
     end
-  end
-
-  controller :main do
-    get '/' => :index
-    get 'developer' => :developer
-    get ':release/download.js' => :download_js
-    get 'main/download' => :download
-
-    get '421' => :release, :release => "421", :outdated => true
-    get '421/:locale' => :release, :release => "421", :outdated => true, :constraints => { :locale => /[\w]+/ }
-    get '132' => :release, :release => "132", :outdated => true
-    get '132/:locale' => :release, :release => "132", :outdated => true, :constraints => { :locale => /[\w]+/ }
-    get '131' => :release, :release => "131", :outdated => true
-    get '131/:locale' => :release, :release => "131", :outdated => true, :constraints => { :locale => /[\w]+/ }
-    get '123' => :release, :release => "123", :outdated => true
-    get '123/:locale' => :release, :release => "123", :outdated => true, :constraints => { :locale => /[\w]+/ }
-    get '122' => :release, :release => "122", :outdated => true
-    get '122/:locale' => :release, :release => "122", :outdated => true, :constraints => { :locale => /[\w]+/ }
-    get '121' => :release, :release => "121", :outdated => true
-    get '121/:locale' => :release, :release => "121", :outdated => true, :constraints => { :locale => /[\w]+/ }
-    get ':release' => :releasemain, :constraints => { :release => /[1234][\d]+/ }, :format => false
-    get ':release/:locale' =>  :release, :constraints => { :release => /[1234][\d]+/, :locale => /[\w]+/ }, :format => false
-    get 'developer/:locale' => :release, :format => false, :constraints => { :locale => /[\w]+/ }
-
-    get 'change_install' => :change_install
-
-    get 'ymp/:project/:repository/:package.ymp' => :ymp_without_arch_and_version,
-          :constraints => { :project => /[\w\-\.:\+]+/, :repository => /[\w\-\.:\+]+/, :package => /[-+\w\.:\@]+/ }
-    get 'ymp/:project/:repository/:arch/:binary.ymp' => :ymp_with_arch_and_version,
-          :constrains => { :project => /[\w\-\.:]+/, :repository => /[\w\-\.:]+/, :arch => /[\w\-\.:]+/, :binary => /[\w\-\.:\+]+/ }
   end
 
   controller :search do
@@ -64,5 +37,4 @@ SoftwareOO::Application.routes.draw do
   get 'download/json' => "download#package", :format => 'json'
 
   get '/download/:action(.:format)', :controller => 'download'
-
 end
