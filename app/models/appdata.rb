ee33492f68b2333923fb0e326a623677d8dbc092
Duncan Mac-Vicar P <dmacvicar@suse.de>
2017-10-19 20:42:40 +0200

    no need to keep comments, we have version control

--- app/models/appdata.rb
+++ app/models/appdata.rb
@@ -6,8 +6,6 @@ class Appdata
     data = Hash.new
     xml = Appdata.get_distribution dist, "oss"
     data = add_appdata data, xml
-#    xml = Appdata.get_distribution dist, "non-oss"
-#    data = add_appdata data, xml
     data
   end
 
