d6b0781dbd302a7cfc1f589c5a151e99cc1e4d05
Duncan Mac-Vicar P <dmacvicar@suse.de>
2017-10-20 23:06:05 +0200

    enable static file serving in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,10 @@
 SoftwareOO::Application.configure do
   config.cache_store = :mem_cache_store, 'localhost:11211', {namespace: 'software', compress: true}
+
   config.log_level = :debug
 
-  config.serve_static_files = false
+  # disabled until we figure the static.opensuse.org magic
+  # config.serve_static_files = false
 
   # Compress JavaScripts and CSS
   config.assets.js_compressor = :uglifier
