fab8d1af37ad0d4d7831f2f00cb563a3a50f0775
Duncan Mac-Vicar P <dmacvicar@suse.de>
2018-02-03 00:27:12 +0100

    Add unit test for app_data model

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,15 +14,16 @@ class ActiveSupport::TestCase
   # test/support
   def stub_remote_file(url, filename)
     %w[http https].each do |protocol|
-      stub_request(:any, "#{protocol}://test:test@#{url}").to_return(body: File.read(Rails.root.join('test', 'support', filename)))
+      stub = stub_request(:any, "#{protocol}://#{url}").to_return(body: File.read(Rails.root.join('test', 'support', filename)))
+      stub.with(basic_auth: ['test', 'test']) if url =~ /^api/
     end
   end
 
   setup do
     # stub OBS using WebMock
     stub_remote_file("api.opensuse.org/public/distributions?", "distributions.xml")
-    stub_remote_file("download.opensuse.org/factory/repo/oss/suse/setup/descr/appdata.xml.gz?", "appdata.xml.gz")
-#    stub_remote_file("download.opensuse.org/factory/repo/non-oss/suse/setup/descr/appdata.xml.gz?", "appdata-non-oss.xml.gz")
+    stub_remote_file("download.opensuse.org/tumbleweed/repo/oss/suse/setup/descr/appdata.xml.gz", "appdata.xml.gz")
+    stub_remote_file("download.opensuse.org/tumbleweed/repo/non-oss/suse/setup/descr/appdata.xml.gz", "appdata-non-oss.xml.gz")
     stub_remote_file("api.opensuse.org/search/published/binary/id?match=@name%20=%20'pidgin'%20", "pidgin.xml")
     stub_remote_file("api.opensuse.org/published/openSUSE:13.1/standard/i586/pidgin-2.10.7-4.1.3.i586.rpm?view=fileinfo", "pidgin-fileinfo.xml")
     stub_request(:get, "https://test:test@api.opensuse.org/source/openSUSE:13.1/_attribute/OBS:QualityCategory").to_return(body: "<attributes/>")
