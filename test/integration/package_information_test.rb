3a2abd3d955c130706e63baf7f4b4b953edd63d0
Duncan Mac-Vicar P <dmacvicar@suse.de>
2018-02-04 00:20:25 +0100

    Replace boolean assertion with a proper content assertion from Capybara
    Gives a much more meanful message if the assertion fails.

--- test/integration/package_information_test.rb
+++ test/integration/package_information_test.rb
@@ -5,7 +5,7 @@ class PackageInformationTest < ActionDispatch::IntegrationTest
   def test_package_information
     # Check that package information is displayed
     visit '/package/pidgin'
-    assert page.has_content? 'Multiprotocol Instant Messaging Client'
-    assert page.has_content? 'Pidgin is a chat program'
+    page.assert_text 'Multiprotocol Instant Messaging Client'
+    page.assert_text 'Pidgin is a chat program'
   end
 end
