8dfdbc8ba5e274a1620883402fe25274b4ff788b
Guo Yunhe <guoyunhebrave@gmail.com>
2018-05-24 10:16:57 +0300

    Fix test

--- test/system/search_results_test.rb
+++ test/system/search_results_test.rb
@@ -4,12 +4,13 @@ class SearchResultsTest < ActionDispatch::SystemTestCase
   def test_default_searches
     stub_search_random('nvidia', 'openSUSE:Leap:42.3')
     visit '/'
-    page.fill_in 'q', with: 'nvidia'
-    page.find(:css, 'button#settings').click
     within '#search-settings' do
       find('option[value="openSUSE:Leap:42.3"]').click
     end
     page.click_on 'OK'
+
+    page.fill_in 'q', with: 'nvidia'
+    page.find(:css, 'button#search-button').click
 
     page.assert_text 'for instructions how to configure your NVIDIA graphics card'
   end
@@ -17,12 +18,13 @@ class SearchResultsTest < ActionDispatch::SystemTestCase
   def test_non_existing_packages
     stub_search_random('paralapapiricoipi', 'openSUSE:Leap:42.3', matches: 0)
     visit '/'
-    page.fill_in 'q', with: 'paralapapiricoipi'
-    page.find(:css, 'button#settings').click
     within '#search-settings' do
       find('option[value="openSUSE:Leap:42.3"]').click
     end
     page.click_on 'OK'
+
+    page.fill_in 'q', with: 'paralapapiricoipi'
+    page.find(:css, 'button#search-button').click
 
     page.assert_text 'No packages found matching your search.'
   end
