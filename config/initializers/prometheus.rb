a5a07582802716e1e31fd7b486a6af70c9b94de3
Frantisek Kobzik <fkobzik@suse.de>
2018-07-01 20:59:06 +0200

    Report data about http requests and ruby vm

---
+++ config/initializers/prometheus.rb
@@ -0,0 +1,10 @@
+require 'prometheus_exporter/middleware'
+
+# This reports stats per request like HTTP status and timings
+Rails.application.middleware.unshift PrometheusExporter::Middleware
+
+require 'prometheus_exporter/instrumentation'
+
+# this reports basic process stats like RSS and GC info, type master
+# means it is instrumenting the master process
+PrometheusExporter::Instrumentation::Process.start(type: "master")
