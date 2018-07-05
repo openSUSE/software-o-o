e64aaafa259b89075e32bbfad569a659afdc3cc5
Guo Yunhe <guoyunhebrave@gmail.com>
2018-07-05 16:37:38 +0300

    Move all settings from modal to search form. Remove buttons.

--- app/assets/javascripts/search-settings.js
+++ app/assets/javascripts/search-settings.js
@@ -1,42 +1,18 @@
 /**
- * Control search settings modal.
- * - Settings will be stored in Cookies, not shown in URL anymore.
- * - Clicking OK button refresh current page.
- * - Clicking Cancel or x button do nothing.
+ * Control search form interaction
  */
 $(function() {
-  var $modal = $("#search-settings");
-  var $form = $modal.find("form");
-  var $ok = $modal.find(".ok-button");
-  var $cancel = $modal.find(".cancel-button");
+  var $form = $("#search-form");
 
-  // For fresh new system, popup modal and let users choose their system version
-  if (!Cookies.get("baseproject")) {
-    $modal.modal("show");
-  }
-
-  function save_input_cookie(name) {
-    var value = $form.find('[name="' + name + '"]').val();
-    console.log(name + ": " + value);
-    Cookies.set(name, value, { expires: 365 });
-  }
-
-  function save_checkbox_cookie(name) {
-    var value = $form.find('[name="' + name + '"]').prop("checked");
-    console.log(name + ": " + value);
-    Cookies.set(name, value, { expires: 365 });
-  }
-
-  $ok.click(function() {
-    save_input_cookie("baseproject");
-    save_checkbox_cookie("search_devel");
-    save_checkbox_cookie("search_lang");
-    save_checkbox_cookie("search_debug");
-    $modal.modal("hide");
+  $form.find('[name="baseproject"]').change(function() {
+    Cookies.set("baseproject", $(this).val(), { expires: 365 });
     location.reload();
   });
-  $cancel.click(function() {
-    $form[0].reset();
-    $modal.modal("hide");
+
+  ["search_devel", "search_lang", "search_debug"].map(function(name) {
+    $form.find('[name="' + name + '"]').change(function() {
+      Cookies.set(name, $(this).prop("checked"), { expires: 365 });
+      location.reload();
+    });
   });
 });
--- test/system/search_results_test.rb
+++ test/system/search_results_test.rb
@@ -2,13 +2,9 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class SearchResultsTest < ActionDispatch::SystemTestCase
   def test_default_searches
-    stub_search_random('nvidia', 'openSUSE:Leap:42.3')
-    visit '/'
-    within '#search-settings' do
-      find('option[value="openSUSE:Leap:42.3"]').click
-    end
-    page.click_on 'OK'
+    stub_search_random('nvidia', 'openSUSE:Leap:15.0')
 
+    visit '/'
     page.fill_in 'q', with: 'nvidia'
     page.find(:css, 'button#search-button').click
 
@@ -16,13 +12,9 @@ class SearchResultsTest < ActionDispatch::SystemTestCase
   end
 
   def test_non_existing_packages
-    stub_search_random('paralapapiricoipi', 'openSUSE:Leap:42.3', matches: 0)
-    visit '/'
-    within '#search-settings' do
-      find('option[value="openSUSE:Leap:42.3"]').click
-    end
-    page.click_on 'OK'
+    stub_search_random('paralapapiricoipi', 'openSUSE:Leap:15.0', matches: 0)
 
+    visit '/'
     page.fill_in 'q', with: 'paralapapiricoipi'
     page.find(:css, 'button#search-button').click
 
