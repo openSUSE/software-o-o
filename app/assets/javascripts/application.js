9ed442a4206b0d43ef3166ee01dd7d05ebffa19b
Duncan Mac-Vicar P <dmacvicar@suse.de>
2018-10-08 08:53:41 +0200

    Remove libraries that seem unused

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,8 @@
 // This is the root JavaScript files. Require other JavaScript files here and
 // they will be bundled together.
 
+//= require jquery3
+//= require chameleon/chameleon
 //= require js-cookie
 //= require search-settings
 //= require common_functionality
