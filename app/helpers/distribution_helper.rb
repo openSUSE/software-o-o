a51c3aa4e90994155ed2cc877e3ebfdee324954f
Stasiek Michalski <hellcp@opensuse.org>
2019-07-08 16:28:40 +0200

    Check image sizes on the fly

--- app/helpers/distribution_helper.rb
+++ app/helpers/distribution_helper.rb
@@ -17,4 +17,14 @@ module DistributionHelper
 
     markdown.render(text).html_safe
   end
+
+  def retrieve_image_size(url)
+    conn = Faraday.new(url: url) do |f|
+      f.use FaradayMiddleware::FollowRedirects
+      f.request  :url_encoded
+      f.adapter  Faraday.default_adapter
+    end
+    # Turn into integer just in case we have dead links (they will report 0B)
+    conn.head.headers['content-length'].to_i
+  end
 end
