ff56c280bedd805803c25276151b00c890e54d5e
Alexander Graul <mail@agraul.de>
2019-08-10 19:28:07 +0200

    Enable asset debugging in development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ SoftwareOO::Application.configure do
   config.cache_store = :memory_store
   # Do not eager load code on boot.
   config.eager_load = false
-
+  config.assets.debug = true
   # Needed for generated thumbnails
   config.serve_static_files = true
 end
