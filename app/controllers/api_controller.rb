4aead22ee5a6606fa21634f5849d75bea6f9714e
Stasiek Michalski <stasiek@michalski.cc>
2020-02-14 01:12:36 +0100

    Add class documentation

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Provides API for usage in the distros
 class ApiController < OBSController
   # Provides info on all the versions of the distributions.
   # GET /api/v0/distributions
