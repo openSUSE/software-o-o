c8d17a26f5dd577ab4b18299e415e0ff3cffebe3
Stasiek Michalski <stasiek@michalski.cc>
2020-02-14 01:08:45 +0100

    Add a comment to the controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApiController < OBSController
+  # Provides info on all the versions of the distributions.
+  # GET /api/v0/distributions
   def distributions
     @leap_versions = load_releases
   end
