f36e77bcdca6620cfd9e7b7f3990c7eca99cc2c9
Sasi Olin <hel@lcp.world>
2022-03-20 00:16:02 +0100

    Replace openstruct properly

--- test/helper/search_helper_test.rb
+++ test/helper/search_helper_test.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 require 'test_helper'
-require 'ostruct'
 
 class SearchHelperTest < ActionView::TestCase
   test 'trust level should be accurate' do
-    assert_equal 3, trust_level(Struct.new(name: 'foo', project: 'openSUSE:Factory'), 'openSUSE:Factory')
-    assert_equal 3, trust_level(Struct.new(name: 'foo', project: 'openSUSE:Factory:Update'), 'openSUSE:Factory')
-    assert_equal 3, trust_level(Struct.new(name: 'foo', project: 'openSUSE:Factory:NonFree'), 'openSUSE:Factory')
-    assert_equal 1, trust_level(Struct.new(name: 'foo', project: 'some:project'), 'openSUSE:Factory')
-    assert_equal 0, trust_level(Struct.new(name: 'foo', project: 'home:bob'), 'openSUSE:Factory')
+    Package = Struct.new(:name, :project)
+    assert_equal 3, trust_level(Package.new('foo', 'openSUSE:Factory'), 'openSUSE:Factory')
+    assert_equal 3, trust_level(Package.new('foo', 'openSUSE:Factory:Update'), 'openSUSE:Factory')
+    assert_equal 3, trust_level(Package.new('foo', 'openSUSE:Factory:NonFree'), 'openSUSE:Factory')
+    assert_equal 1, trust_level(Package.new('foo', 'some:project'), 'openSUSE:Factory')
+    assert_equal 0, trust_level(Package.new('foo', 'home:bob'), 'openSUSE:Factory')
   end
 end
