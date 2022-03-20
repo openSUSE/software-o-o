269b5d690af766f4723edaab0bc0bf3ded3dd9b1
Sasi Olin <hel@lcp.world>
2022-03-20 16:22:13 +0100

    Update rails config to 7.0

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,5 @@
-# Load the rails application
-require File.expand_path('../application', __FILE__)
-GettextI18nRails.translations_are_html_safe = true
+# Load the Rails application.
+require_relative "application"
 
-SoftwareOO::Application.configure do
-  # OBS API access
-  config.x = Hashie::Mash.new(config_for(:options))
-end
-
-# Initialize the rails application
-SoftwareOO::Application.initialize!
+# Initialize the Rails application.
+Rails.application.initialize!
