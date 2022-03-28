db97d5546b1b80ace27a20a9b926c1018891c2f9
Sasi Olin <hel@lcp.world>
2022-03-28 16:38:27 +0200

    Fix the rubocop issues

--- test/integration/download_controller_test.rb
+++ test/integration/download_controller_test.rb
@@ -9,10 +9,13 @@ class DownloadControllerTest < ActionDispatch::IntegrationTest
       assert_response :success
       # Check if the schema is right
       json_item = JSON.parse(response.body).values.first
-      assert_match %r{https://download.opensuse.org/repositories/openSUSE:Tools/(.*)/}, json_item['repo']
+      repo_link = 'https://download.opensuse.org/repositories'
+      assert_match %r{#{repo_link}/openSUSE:Tools/(.*)/}, json_item['repo']
       # The key is filename, the value is link to that file on the server
-      assert_match /osc(.*)/, json_item['package'].keys.first
-      assert_match %r{https://download.opensuse.org/repositories/openSUSE:/Tools/(.*)/#{json_item['package'].keys.first}}, json_item['package'].values.first
+      package_name = json_item['package'].keys.first
+      package_link = json_item['package'].values.first
+      assert_match(/osc(.*)/, package_name)
+      assert_match %r{#{repo_link}/openSUSE:/Tools/(.*)/#{package_name}}, package_link
     end
   end
 end
