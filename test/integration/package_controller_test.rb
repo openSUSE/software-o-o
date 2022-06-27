fc79b6a6d6d269d894bb8c457cd6e41044697e0b
Henne Vogelsang <hvogel@opensuse.org>
2022-06-27 15:47:28 +0200

    Merge OBSControllerTest into PackageControllerTest

--- test/integration/package_controller_test.rb
+++ test/integration/package_controller_test.rb
@@ -4,6 +4,7 @@ require File.expand_path('../test_helper', __dir__)
 require 'minitest/mock'
 require 'digest'
 require 'fileutils'
+require 'api_connect'
 
 class PackageControllerTest < ActionDispatch::IntegrationTest
   UNKNOWN_PACKAGE_THUMBNAIL = Rails.root.join('public', 'images', 'thumbnails', 'SuperFancyBrowser.png')
@@ -67,6 +68,32 @@ class PackageControllerTest < ActionDispatch::IntegrationTest
     VCR.use_cassette('unknown screenshot is 404') do
       get '/package/screenshot/paralapapiricoipi.png?baseproject=ALL'
       assert_equal 404, status
+    end
+  end
+
+  test 'backend_connection' do
+    VCR.use_cassette('backend connection') do
+      get '/explore'
+
+      assert_includes body, 'Search packages...'
+      assert_equal 200, status
+    end
+  end
+
+  test 'backend error handling' do
+    mock = Minitest::Mock.new
+    def mock.get
+      raise Error
+    end
+
+    ApiConnect.stub :get, mock do
+      # Otherwise the controller would get the distributions from cache
+      Rails.cache.clear
+      VCR.use_cassette('backend error handling') do
+        get '/explore'
+        assert_includes body, 'Connection to OBS is unavailable.'
+        assert_equal 200, status
+      end
     end
   end
 end
