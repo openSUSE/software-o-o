ff94216c7f3fa0effdb80014f296a6573267e66a
Henne Vogelsang <hvogel@opensuse.org>
2023-11-10 17:15:46 +0100

    Add database seeds

--- app/models/distribution.rb
+++ app/models/distribution.rb
@@ -6,6 +6,7 @@ class Distribution < ApplicationRecord
   has_many :packages, through: :repositories
 
   validates :name, :obs_repo_names, presence: true
+  validates :name, uniqueness: true
 
   def sync
     repositories.where(updateinfo: false).find_each(&:sync)
