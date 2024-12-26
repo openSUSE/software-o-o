b5ab2c84b225d669b49a527833c22c67e225fc26
Aman Pal <amanpal1586@gmail.com>
2024-12-26 12:20:49 +0530

    Update common_functionality.js
    Replaced all instances of var with let to improve code readability and align with modern JavaScript best practices. This change ensures block-scoping and avoids potential issues with variable hoisting.

--- app/assets/javascripts/common_functionality.js
+++ app/assets/javascripts/common_functionality.js
@@ -1,5 +1,5 @@
 // Preselect a tab when it's referenced in url
 $(document).ready(function () {
-  var hash = window.location.hash;
+  let hash = window.location.hash;
   hash && $('ul.nav a[href="' + hash + '"]').tab('show');
 });
