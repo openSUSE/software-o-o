a16104c456243252bed08727bbc305dff76be9ff
Henne Vogelsang <hvogel@opensuse.org>
2025-12-05 13:04:37 +0100

    UI WIP

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 class MainController < ApplicationController
+  MAIN_CATEGORIES = %w[Game Development Education Science Multimedia Graphics Office Network Settings Utility].freeze
+
   def index
     # FIXME: This should be the distro we want to show by default, not the first...
     @distribution = Distribution.first
+    @packages = Package.joins(:screenshots_blobs).where(repository: @distribution.repositories).last(4)
+    @categories = @distribution.categories.where(name: MAIN_CATEGORIES)
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,21 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :packages
   validates :name, presence: true
+
+  def related
+    packages.map(&:categories).flatten.uniq
+  end
+
+  def icon
+    { Game: 'fa-gamepad',
+      Development: 'fa-code',
+      Education: 'fa-graduation-cap',
+      Science: 'fa-microscope',
+      Multimedia: 'fa-circle-play',
+      Graphics: 'fa-image',
+      Office: 'fa-file-lines',
+      Network: 'fa-wifi',
+      Settings: 'fa-gear',
+      Utility: 'fa-screwdriver-wrench' }.with_indifferent_access[name]
+  end
 end
